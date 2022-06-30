31aa640db3da859a2f6202c2a630451f9b1f19fe
Thomux <me@homux.eu>
2022-06-30 12:03:43 +0200

    multi-threaded server

---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+use std::error;
+use std::fmt;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+
+#[derive(Debug)]
+pub struct PoolCreationError;
+
+impl error::Error for PoolCreationError {}
+
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "PoolCreationError")
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size <= 0 {
+            return Err(PoolCreationError);
+        }
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        Ok(ThreadPool { workers, sender })
+    }
+}
+
+impl ThreadPool {
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job; executing.", id);
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use hello::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::process;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap_or_else(|e| {
@@ -10,15 +13,19 @@ fn main() {
         process::exit(1);
     });
 
+    let pool = ThreadPool::new(4).unwrap();
+
     for stream in listener.incoming() {
         let stream = stream.unwrap_or_else(|e| {
             println!("Error: {}", e);
             process::exit(1);
         });
 
-        if let Err(e) = handle_connection(stream) {
-            println!("{})", e);
-        };
+        pool.execute(|| {
+            if let Err(e) = handle_connection(stream) {
+                println!("{})", e);
+            };
+        });
     }
 }
 
@@ -27,8 +34,12 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Er
     stream.read(&mut buffer)?;
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
